93e10bca69c38b6b818a50141e135ce2879364e2
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-07 20:50:30 +0000

    Added a function to see if a list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a list has a cycle or not
+ * @list: pointer to list structure
+ * Return: 0 if list has no cycle, else 1.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+
+	while (list->next)
+	{
+		if (list->next == head)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}

3294d852688f81f9a28a138470849e82b3d146b1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-07 15:53:42 -0500

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,11 +9,14 @@ int check_cycle(listint_t *list)
 {
 	listint_t *head = list;
 
-	while (list->next)
+	if (head)
 	{
-		if (list->next == head)
-			return (1);
-		list = list->next;
+		while (list->next)
+		{
+			if (list->next == head)
+				return (1);
+			list = list->next;
+		}
 	}
 	return (0);
 }

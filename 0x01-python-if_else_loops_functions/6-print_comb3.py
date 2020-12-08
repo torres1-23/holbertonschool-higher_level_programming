e94863a23e55315f9519bb50ce45503da94587e8
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-08 23:53:35 +0000

    Fix file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for n in range(0, 10):
+for n in range(0, 10-m ""):
     for nu in range(0, 10):
         if nu > n and n != 8:
             print("{:d}{:d}".format(n, nu), end=", ")

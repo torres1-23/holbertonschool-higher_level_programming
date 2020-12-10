d63bcd2d02e2a9fb0dd358e0cd67fa3a0f0b0132
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-10 20:49:10 +0000

    Fix 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import dis
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -11,6 +8,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return(sub(a, b))
-
-
-dis.dis(magic_calculation)

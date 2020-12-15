f391df9839a383b93b393e176500888e4c05095e
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-15 15:09:56 -0500

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@ int is_palindrome(listint_t **head)
 	listint_t *tmp = NULL;
 	int *num = NULL, i, j;
 
-	if (*head == NULL)
+	if (*head == NULL && head == NULL)
 		return (1);
 	tmp = *head;
 	for (i = 0; tmp; i++)

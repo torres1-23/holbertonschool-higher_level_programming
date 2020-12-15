f2dc1dd1e0771af5734f56548973a9cf9a2c2704
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-15 15:05:53 -0500

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,33 +11,32 @@ int is_palindrome(listint_t **head)
 	listint_t *tmp = *head;
 	int *num = NULL, i, j;
 
-	if (*head)
+	if (*head == NULL)
+		return (1);
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
+	if (i == 1)
+		return (1);
+	if (i % 2 != 0)
+		return (0);
+	num = malloc(sizeof(int) * i);
+	if (!num)
+		return (-1);
+	tmp = *head;
+	for (j = 0; tmp; j++)
 	{
-		for (i = 0; tmp; i++)
-			tmp = tmp->next;
-		if (i <= 1)
-			return (1);
-		if (i % 2 != 0)
-			return (0);
-		num = malloc(sizeof(int) * i);
-		if (!num)
-			return (-1);
-		tmp = *head;
-		for (j = 0; tmp; j++)
-		{
-			num[j] = tmp->n;
-			tmp = tmp->next;
-		}
-		for (j = 0; j < i - 1; j++)
-		{
-			if (num[j] != num[i - 1])
-			{
-				free(num);
-				return (0);
-			}
-			i--;
-		}
-		free(num);
+		num[j] = tmp->n;
+		tmp = tmp->next;
 	}
+	for (j = 0; j < i - 1; j++)
+	{
+		if (num[j] != num[i - 1])
+		{
+			free(num);
+			return (0);
+		}
+		i--;
+	}
+	free(num);
 	return (1);
 }

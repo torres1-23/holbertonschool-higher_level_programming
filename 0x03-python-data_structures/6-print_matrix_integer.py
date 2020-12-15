15f78738e99dc2170e8ab596347fd2bb6f9e7ed4
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-15 20:22:42 +0000

    Fix file 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """ Prints a matrix of integers. """
-    if matrix != [[]]:
+    if matrix != []:
         for row in matrix:
-            for n in row:
+            for n in row[:-1]:
                 print("{:d}".format(n), end=" ")
-            print("")
+            print("{:d}".format(row[-1]))
     else:
         print("")

965406a2b10a8c57077dbf117f5cbfb1eb603ec2
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-17 19:47:42 +0000

    Fix file 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[pow(n, 2) for n in row] for row in matrix]
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda n: n ** 2, row[:])), matrix))

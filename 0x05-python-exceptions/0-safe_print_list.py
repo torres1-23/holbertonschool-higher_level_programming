156fae526202e611ab603b1d6f9e315f36ecfccb
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-04 17:19:14 +0000

    Fix handle of all possible exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,7 @@
 
 
 def safe_print_list(my_list=[], x=0):
+
     """ Prints x elements of a list.
     Args:
         my_list (list): list to print.
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -13,6 +13,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err_msg:
+    except (TypeError, ValueError) as err_msg:
         print("Exception: {}".format(err_msg), file=sys.stderr)
         return False

0233c2675452df8c6686e9f2743a40d81da6b9f5
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-05 17:09:25 +0000

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Define class Square. """
+
+
+class Square:
+    """ Represents a square. """
+    def __init__(self, size):
+        """ Initializes instance of square.
+        Args:
+            size (int): size of square.
+        Attributes:
+            __size (int): size of square.
+        """
+        self.__size = size

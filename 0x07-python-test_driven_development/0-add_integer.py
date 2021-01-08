29812120f81fecbfceb4b0ca03f6bb25116ef1dc
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-08 22:31:55 +0000

    Added task 0 and test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This module defines an integer addition function "add_integer(a, b=98)"
+
+Usage:
+    "add_integer(...)" return the addition of two arguments,
+    equivalent to using the "+" operator in numbers.
+"""
+
+
+def add_integer(a, b=98):
+    """Function that adds 2 integers.
+
+    Args:
+        a (int): first number to add.
+        b (int, optional): second number to add.
+
+    Raises:
+        TypeError: if a or b are not integers nor floats
+
+    Returns:
+        int: Addition of the two integers.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)

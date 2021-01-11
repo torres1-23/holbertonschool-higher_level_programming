1d8aa785709ab15336466c1a0f41266cc106d99d
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-11 16:57:22 +0000

    Add task 0 solution

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module defines an empty class Rectangle.
+
+Usage:
+    Class Rectangle is used to instanciate empty rectangles.
+"""
+
+
+class Rectangle:
+    """Represent an empty rectangle."""
+    pass

19e2396611f3fa59ccf7077f27219a7433db382b
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-11 20:56:38 +0000

    Add solution to task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -110,7 +110,8 @@ class Rectangle:
         """Defines the printable representation of a rectangle.
 
         Returns:
-            String representation of a rectangle using the current instance symbol.
+            String representation of a rectangle
+            using the current instance symbol.
         """
         string = ""
         if self.width != 0 and self.height != 0:
@@ -134,3 +135,23 @@ class Rectangle:
         """Deletes a rectangle instance and print a message"""
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Compares two instamces of rectangle.
+
+        Returns:
+            The instance with the biggest area, if both areas
+            are equal returns the first instance.
+
+        Raises:
+            TypeError:
+            TypeError:
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2

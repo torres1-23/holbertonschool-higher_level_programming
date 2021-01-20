e120f47adbb000aa0c5d6527234eb690918356db
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-20 02:42:06 +0000

    Add file for task 7 solution

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,17 @@ class Rectangle(BaseGeometry):
     """Represents a rectangle, inherits from class BaseGeometry."""
 
     def __init__(self, width, height):
+        """Initializes instances of Rectangle.
+
+        Args:
+            width (int): width of rectangle instance.
+            height (int): height of rectangle istance.
+
+        Attributes:
+            __width (int): width of rectangle instance.
+            __height (int): height of rectangle instance.
+        """
         self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("heigth", height)
+        self.integer_validator("height", height)
         self.__height = height

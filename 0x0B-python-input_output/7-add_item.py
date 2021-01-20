dc2c17218d1d019c6c4e2282b69654eae11b3a26
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-20 22:34:09 +0000

    Add file for task 7 solution

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This script writes to a .json file the arguments given in the stdin
+
+Usage:
+    All the arguments in the stdin after the executable name will be added to
+    a list and then added to a file "add_item.json", if the file was already
+    created, the list will be appended.
+"""
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+try:
+    lst = load_from_json_file("add_item.json")
+except Exception:
+    lst = []
+lst += sys.argv[1:]
+save_to_json_file(lst, "add_item.json")

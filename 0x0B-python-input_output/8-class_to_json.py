20a929c5ad98b79a00208641ba5d18fd8b243820
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-20 23:04:56 +0000

    Fix file for task 8 solution

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 """This module defines a function "def class_to_json(obj)",
-that reads a file and gets a JSON string.
+that reads a file and gets the dictionary of an object.
 
 Usage:
     "def class_to_json(...)" receives an object "obj" and
-    returns its dictionary as a JSON string.
+    returns its dictionary.
 """
-import json
 
 
 def class_to_json(obj):
@@ -21,4 +20,4 @@ def class_to_json(obj):
     Return:
         JSON string of dictionary.
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__

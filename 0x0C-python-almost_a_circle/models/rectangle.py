7506519d4783fe961de319aa89707609c347f9a0
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-22 17:08:13 +0000

    Added file rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+"""This module defines a class "class Rectangle"."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """This class defines instaces of rectangle, inherits from class "Base"."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance of rectangle.
+
+        Args:
+            width (int): width of each instance.
+            height (int): height of each instance.
+            x (int, optional): attribute x of each instance,
+            0 by default.
+            y (int, optional): attribute y of each instance,
+            0 by default.
+            id (int, optional): id of each instance,
+            None by default.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Gets width attribute of each instance.
+
+        Return:
+            width of each instance.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width attribute of each instance.
+
+        Args:
+            value (int): value to set.
+
+        Attributes:
+            width (int): width of each instance.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets height attribute of each instance.
+
+        Return:
+            height of each instance.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height attribute of each instance.
+
+        Args:
+            value (int): value to set.
+
+        Attributes:
+            height (int): height of each instance.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """Gets x attribute of each instance.
+
+        Return:
+            x of each instance.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x attribute of each instance.
+
+        Args:
+            value (int): value to set.
+
+        Attributes:
+            x (int): x of each instance.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """Gets y attribute of each instance.
+
+        Return:
+            y of each instance.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y attribute of each instance.
+
+        Args:
+            value (int): value to set.
+
+        Attributes:
+            y (int): y of each instance.
+        """
+        self.__y = value

b7f79b98f0f98eb2a4293c8fce3353e66c6155bf
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-25 00:39:35 +0000

    Add method to save JSON dict of instances to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,20 @@ class Base:
         if list_dictionaries is None or not bool(list_dictionaries):
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): List of dictionaries of instances to save.
+        """
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, mode='w', encoding='UTF8') as s_file:
+            if list_objs is None:
+                s_file.write('[]')
+            else:
+                dic_list = []
+                for ins in list_objs:
+                    dic_list.append(ins.to_dictionary())
+                s_file.write(Base.to_json_string(dic_list))

12760417566d93cbbbb03e461545d3e9ac21a69a
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-27 14:14:37 +0000

    Add missing test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -225,6 +225,9 @@ class TestBase03(unittest.TestCase):
         Square.save_to_file([])
         with open("Square.json", "r") as f:
             self.assertEqual("[]", f.read())
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
 
     def test_04(self):
         """Checks exception raises."""

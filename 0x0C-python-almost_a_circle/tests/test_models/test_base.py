5959199430089666a9e05a3ee3549cf9b868e494
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-27 14:36:38 +0000

    Add missing test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -219,20 +219,24 @@ class TestBase03(unittest.TestCase):
     def test_03(self):
         """Check if list saved is OK when
         no argument."""
-        Square.save_to_file(None)
-        with open("Square.json", "r") as f:
-            self.assertEqual("[]", f.read())
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as f:
             self.assertEqual("[]", f.read())
         Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as f:
             self.assertEqual("[]", f.read())
+
+    def test_04(self):
+        """Check if list saved is OK when
+        no argument."""
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
         Square.save_to_file([])
         with open("Square.json", "r") as f:
             self.assertEqual("[]", f.read())
 
-    def test_04(self):
+    def test_05(self):
         """Checks exception raises."""
         with self.assertRaises(TypeError):
             Rectangle.save_to_file()

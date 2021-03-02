740791bfa98105c199baa671d746e5ae2bf57631
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-02 20:54:02 +0000

    Add script to create a user with password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,8 @@
 -- Creates a user in a MYSQL server.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

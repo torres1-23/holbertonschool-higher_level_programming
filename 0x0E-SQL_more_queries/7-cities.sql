323004df4a9bf8eafe837575bd4bea4d33392881
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-02 22:14:11 +0000

    Add script that creates cities table in hbtn_od_usa database

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- Creates a database hbtn_0d_usa.
+-- Table cities
+-- Columns id INT, unique, auto, not null and is a primary key.
+-- state_id INT, not null and must be a FOREIGN KEY that references to id of the states table.
+-- name 256 bytes long not null.
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE
+    IF NOT EXISTS hbtn_0d_usa.cities(
+        id INT UNIQUE NOT NULL AUTO_INCREMENT,
+        state_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (state_id)
+            REFERENCES states(id)
+    );

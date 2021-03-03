310870c3f560ce3636bc69014df9a02ff5ed9b5d
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-03 00:43:21 +0000

    Add script that list all shows and genre name

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Lists all shows and all genres linked to that show from a database passed as argument.
+-- If a show doesn't have genre display NULL.
+-- Display records by: tv_shows.title - tv_genres.name.
+-- Order results by: show title and gender name in ascending order.
+SELECT tv_shows.title, tv_genres.name
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+ ORDER BY tv_shows.title, tv_genres.name;

d2a4e8662dd99e7753351688bb0b1be47c908ef5
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-22 17:07:48 +0000

    Add script that prints a number passed as a console argument

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+  Script that prints My number: <first argument converted in integer>
+  if the first argument can be converted to an integer.
+*/
+if (+process.argv[2]) {
+  console.log('My number: ' + process.argv[2]);
+} else {
+  console.log('Not a number');
+}

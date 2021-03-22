ee89bcec44c26540aa18069252ca40cdfe233bdf
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-22 17:08:24 +0000

    Add script that prints elements of an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Script that prints 3 lines by using an array of string and a loop.
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const string of myArray) {
+  console.log(string);
+}

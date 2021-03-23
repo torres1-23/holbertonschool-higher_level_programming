b748bca2ea620ac4e5e5a0a9fb8f7efc6f9ce172
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-23 18:19:37 +0000

    Add script that creates a Rectangle with validation and a method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Class Rectangle that defines a rectangle.
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let string = '';
+      for (let j = 0; j < this.width; j++) {
+        string += 'X';
+      }
+      console.log(string);
+    }
+  }
+};

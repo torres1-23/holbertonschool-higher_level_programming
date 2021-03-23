2a10206db4316059371c0a8529c1144cc63af46b
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-23 21:40:25 +0000

    Add script that creates a new object based on another one

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const key in dict) {
+  if (!(dict[key] in newDict)) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);

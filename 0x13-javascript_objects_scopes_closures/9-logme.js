e6207432c03c7a4a96559a8130ae02447926e41c
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-23 20:14:45 +0000

    Add fucntion that logs the printed items

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Function that prints the number of arguments already printed and the new argument value.
+let cont = 0;
+exports.logMe = function (item) {
+  console.log(cont++ + ': ' + item);
+};

f70389574a82de8862cef3daf7b2658540a6f6d3
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-25 19:13:02 +0000

    Add script that gets rows of a databse table with a regex

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module list all states with a name starting with N from a database.
+
+Usage:
+    Takes 3 arguments: 'mysql username', 'mysql password' and 'database name'
+    and lists all states from a table in a database using MYSQLdb module.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT *"
+                   "  FROM states"
+                   "  WHERE name REGEXP '^N'")
+    for row in cursor.fetchall():
+        print(row)

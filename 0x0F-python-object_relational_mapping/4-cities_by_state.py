18b59a1719c404f3b426798e6c15f2e683aee309
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-25 20:28:08 +0000

    Add script that executes a query with join in a db

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module takes in an argument and displays all values.
+
+Usage:
+    Takes 3 arguments: 'mysql username', 'mysql password' 'database name'
+    and lists all states from a table in a database using MYSQLdb module.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name"
+                   "  FROM cities"
+                   "  INNER JOIN states"
+                   "    ON cities.state_id = states.id")
+    for row in cursor.fetchall():
+        print(row)

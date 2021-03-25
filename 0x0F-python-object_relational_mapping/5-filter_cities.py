4ae8fa97e3e3510bd1547013e47717ad268d66a4
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-25 21:10:57 +0000

    Add script that displays cities of a state as argument

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module takes in arguments and displays all cities of a given state
+from a database.
+
+Usage:
+    Takes 4 arguments: 'mysql username', 'mysql password' 'database name'
+    and 'state name' and lists all cities of the 'state name'
+    from a table in a database using MYSQLdb module.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    myList = []
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name"
+                   "  FROM cities"
+                   "  INNER JOIN states"
+                   "    ON cities.state_id = states.id"
+                   "  WHERE states.name = %s", (sys.argv[4], ))
+    for row in cursor.fetchall():
+        myList.append(row[0])
+    print(', '.join(myList))

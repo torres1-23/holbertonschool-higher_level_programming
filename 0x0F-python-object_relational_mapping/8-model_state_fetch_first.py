a4aa1bb70e0da9adcc82c9fe2609aa9dd4a8a661
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-25 23:19:11 +0000

    Add script that list the first row of a table with sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module lists the first row of the table states from the database
+hbtn_0e_6_usa using module SQLAlchemy.
+
+Usage:
+    Should take 3 arguments: 'mysql username', 'mysql password' and
+    'database name' and list the first row of states table.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print('Nothing')

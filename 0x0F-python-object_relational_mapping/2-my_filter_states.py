d93acc46c2b052e3e562328fc998159371d6ae48
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-25 19:27:14 +0000

    Add script that searches by name passed as a console argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""This module takes in an argument and displays all values
+where name matches the argument.
+
+Usage:
+    Takes 4 arguments: 'mysql username', 'mysql password' 'database name'
+    and 'state name searched' and lists all states from a table in a database
+    using MYSQLdb module.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT *"
+                   "  FROM states"
+                   "  WHERE name = '{}'".format(sys.argv[4]))
+    for row in cursor.fetchall():
+        print(row)

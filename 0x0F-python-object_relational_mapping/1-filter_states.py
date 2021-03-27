1764e5dcdb3cec922dc6761ab186904d79d5b9f1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-27 16:17:48 +0000

    Add case sensitive REGEXP

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute("SELECT *"
                    "  FROM states"
-                   "  WHERE name REGEXP '?^N'"
+                   "  WHERE name REGEXP BINARY '^N'"
                    "  ORDER BY states.id")
     for row in cursor.fetchall():
         print(row)

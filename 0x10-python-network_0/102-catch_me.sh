ad33e6c9e37dfc3039ab16e4b1dcb2d918df8f6c
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-08 22:04:39 +0000

    Add script that catch a request

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sLH "Origin: HolbertonSchool" -X PUT -d "user_id=98" "$1"
+# Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!.
+curl -sLH "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me

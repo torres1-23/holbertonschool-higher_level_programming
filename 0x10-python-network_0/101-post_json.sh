7b6adb4ce08c576fb1f557bbdb465533748208fc
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-04-08 21:24:56 +0000

    Add script that sends the contentes of a file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -X POST --data-binary @"$2" "$1"
+curl -X POST -d "data=@"$2" "$1"

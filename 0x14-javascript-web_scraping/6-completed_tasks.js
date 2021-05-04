dd9356cd730f153e6db4f7ce645bbe326b3c73a0
torres1-23 <alejo.torres1247@gmail.com>
2021-05-04 23:53:31 +0000

    Add script that creates a JSON object with info from users

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,10 +6,9 @@ request.get(process.argv[2], function (err, res, body) {
   if (!err) {
     const dict = JSON.parse(body);
     for (const i in dict) {
-      if (object[dict[i].userId] === undefined) {
-        object[dict[i].userId] = 0;
-      }
-      if (dict[i].completed) {
+      if (object[dict[i].userId] === undefined && dict[i].completed) {
+        object[dict[i].userId] = 1;
+      } else if (dict[i].completed) {
         object[dict[i].userId] += 1;
       }
     }

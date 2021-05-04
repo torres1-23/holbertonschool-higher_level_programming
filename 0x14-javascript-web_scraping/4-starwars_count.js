02cb2fc443366fb14abc1b65e515e44adf356915
torres1-23 <alejo.torres1247@gmail.com>
2021-05-04 20:53:18 +0000

    Add script that lists how many times a character has appeared in a movie

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,13 @@
 // the character “Wedge Antilles” is present.
 const request = require('request');
 let cont = 0;
-const id = 18;
 request.get(process.argv[2], function (err, res, body) {
   if (!err) {
     const moviesList = JSON.parse(body).results;
     for (const i in moviesList) {
       const charactersList = moviesList[i].characters;
       for (const j in charactersList) {
-        if (charactersList[j].includes(d)) {
+        if (charactersList[j].includes('18')) {
           cont++;
         }
       }

ae94394259e28d566a5b94ca9062e69c7cf999bf
torres1-23 <alejo.torres1247@gmail.com>
2021-05-04 20:49:34 +0000

    Add script that lists how many times a character has appeared in a movie

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,14 @@
 // the character “Wedge Antilles” is present.
 const request = require('request');
 let cont = 0;
-request(process.argv[2], function (err, res, body) {
+request.get(process.argv[2], function (err, res, body) {
   if (!err) {
     const moviesList = JSON.parse(body).results;
     for (const i in moviesList) {
       const charactersList = moviesList[i].characters;
       for (const j in charactersList) {
         if (charactersList[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
-          cont += 1;
-          break;
+          cont++;
         }
       }
     }

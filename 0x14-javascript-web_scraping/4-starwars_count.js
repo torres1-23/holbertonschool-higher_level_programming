f994ac2ff823f3dccf41ae780ebed162460628df
torres1-23 <alejo.torres1247@gmail.com>
2021-05-04 20:46:04 +0000

    Add script that lists how many times a character has appeared in a movie

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,17 @@
 const request = require('request');
 let cont = 0;
 request(process.argv[2], function (err, res, body) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const moviesDict = JSON.parse(body).results;
-  for (const i in moviesDict) {
-    const charactersList = moviesDict[i].characters;
-    for (const j in charactersList) {
-      if (charactersList[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
-        cont += 1;
-        break;
+  if (!err) {
+    const moviesList = JSON.parse(body).results;
+    for (const i in moviesList) {
+      const charactersList = moviesList[i].characters;
+      for (const j in charactersList) {
+        if (charactersList[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          cont += 1;
+          break;
+        }
       }
     }
+    console.log(cont);
   }
-  console.log(cont);
 });
